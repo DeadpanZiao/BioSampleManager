3f1513c7539f90b6f5d5f7b2cd06ac34920cb7c9
a896900806 <Lzz_kugua@163.com>
2024-06-19 14:18:54 +0800

    2.0

--- Fetcher/SingleCellDBs/SingleCellPortal/single_cell_portal.py
+++ Fetcher/SingleCellDBs/SingleCellPortal/single_cell_portal.py
@@ -17,8 +17,6 @@ class SingleCellPortalFetcher(SingleCellDBFetcher):
         response = requests.get(self.datasets_url, headers=self.headers, verify=False)
         if response.status_code == 200:
             studies = response.json()
-            manager = JsonManager(db_name)
-            manager.save(studies)
             self.logger.info("Data saved successfully to JSON file.")
             merged_data = {}
             for study in studies:
@@ -31,8 +29,7 @@ class SingleCellPortalFetcher(SingleCellDBFetcher):
                     self.logger.info(f"Data saved successfully to {accessions}.json file.")
                 else:
                     self.logger.error(f"Failed to retrieve study {accessions}. Status code: {response.status_code}")
-            manager = JsonManager('date')
+            manager = JsonManager(db_name)
             manager.save(merged_data)
         else:
             self.logger.error(f"Failed to retrieve studies. Status code: {response.status_code}")
-
