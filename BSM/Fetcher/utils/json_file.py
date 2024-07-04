59a80d28a7b46284ff26e881f03927f5d773d8e6
Relaxxxxx <liu960929@163.com>
2024-07-04 14:50:52 +0800

    json manager update

--- BSM/Fetcher/utils/json_file.py
+++ BSM/Fetcher/utils/json_file.py
@@ -1,4 +1,5 @@
 import json
+import ijson
 
 class JsonManager(object):
     def __init__(self,  filename):
@@ -7,3 +8,22 @@ class JsonManager(object):
     def save(self, data):
         with open(self.filename, 'w') as file:
             json.dump(data, file, indent=4)
+
+
+    def write_large_json(self, data):
+        with open(self.filename, 'w', encoding='utf-8') as f:
+            f.write('[\n')
+            first = True
+            for item in data:
+                if not first:
+                    f.write(',\n')
+                json.dump(item, f, ensure_ascii=False)
+                first = False
+            f.write('\n]')
+
+    def read_large_json(self):
+        items = []
+        with open(self.filename, 'r', encoding='utf-8') as f:
+            for item in ijson.items(f, 'item'):
+                items.append(item)
+        return items
