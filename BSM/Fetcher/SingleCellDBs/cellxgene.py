5190e49a37009712d433e7166fb01845d511fe0e
Relaxxxxx <liu960929@163.com>
2024-07-08 09:45:56 +0800

    cellxgene update

--- BSM/Fetcher/SingleCellDBs/cellxgene.py
+++ BSM/Fetcher/SingleCellDBs/cellxgene.py
@@ -4,11 +4,12 @@ from BSM.Fetcher.utils import JsonManager
 
 
 class CellxgeneFetcher(SingleCellDBFetcher):
-    def __init__(self, domain_name="cellxgene.cziscience.com", datasets_path="/curation/v1/datasets"):
+    def __init__(self, domain_name="cellxgene.cziscience.com/curation/v1"):
         super().__init__()
         self.domain_name = domain_name
         self.api_url_base = f"https://api.{domain_name}"
-        self.datasets_url = f"{self.api_url_base}{datasets_path}"
+        self.datasets_url = f"{self.api_url_base}/datasets"
+        self.collections_url = f"{self.api_url_base}/collections"
         self.headers = {"Content-Type": "application/json"}
 
     def fetch(self, db_name):
@@ -19,5 +20,13 @@ class CellxgeneFetcher(SingleCellDBFetcher):
         manager = JsonManager(db_name)
         manager.save(data)
         self.logger.info("Data saved successfully to JSON file.")
-f = CellxgeneFetcher()
-f.fetch('db.json')
+
+    def fetch_collections(self, db_name):
+        res = requests.get(url=self.datasets_url, headers=self.headers)
+        res.raise_for_status()
+        data = res.json()
+
+        manager = JsonManager(db_name)
+        manager.save(data)
+        self.logger.info("Data saved successfully to JSON file.")
+    
