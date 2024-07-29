a1ba50d68d97063046134b120de7e633c2068815
Relaxxxxx <liu960929@163.com>
2024-07-29 17:01:40 +0800

    database dev

--- BSM/DataAccess/data_access.py
+++ BSM/DataAccess/data_access.py
@@ -1,6 +1,7 @@
 import sqlite3
 import logging
 
+
 class DataAccess:
     def __init__(self, db_name):
         self.conn = sqlite3.connect(db_name)
@@ -110,19 +111,352 @@ class DataAccess:
         self.conn.close()
         self.logger.info('Database connection closed')
 
-# db_name = '../../DBS/test.db'
-# db = DataAccess(db_name)
-# # 准备要插入的数据
-# data_to_insert = {
-#     'collection_name': 'Sample Atlas',
-#     'collection_id': 'abc123',
-#     'publication_title': 'A study on...',
-#     'publication_doi': '10.1001/jama.2021.12345',
-#     'organism': 'Homo sapiens',
-#     'tissue': 'Brain',
-#     'tech': 'RNA-seq',
-#     'rna_source': 'Nuclei',
-#     'public': True
-# }
-# db.insert(data_to_insert)
-# res = db.query()
+
+class ProjectAccess:
+    def __init__(self, db_name):
+        self.conn = sqlite3.connect(db_name)
+        self.cursor = self.conn.cursor()
+        self.table_name = 'publications'
+        self.setup_logging()
+        self.create_table()
+        self.publication_columns = [
+            "doi", "pmid", "pmcid", "dataset", "title", "authors", "issn", "eissn",
+            "epub_date", "journal_full_name", "journal", "pub_date", "pub_type",
+            "abstract", "keywords", "pub_status", "mesh_term", "fulltext_link", "topic"
+        ]
+
+    def setup_logging(self):
+        logging.basicConfig(filename='data_access.log', level=logging.INFO,
+                            format='%(asctime)s - %(levelname)s - %(message)s')
+        self.logger = logging.getLogger(__name__)
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(logging.INFO)
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+        console_handler.setFormatter(formatter)
+        self.logger.addHandler(console_handler)
+
+    def create_table(self):
+        """
+        创建一个名为 'publications' 的表，并设置 'doi' 为主键。
+        """
+        self.cursor.execute("""
+            CREATE TABLE IF NOT EXISTS publications (
+                doi TEXT PRIMARY KEY,
+                pmid INTEGER,
+                pmcid TEXT,
+                dataset TEXT,
+                title TEXT,
+                authors TEXT,
+                issn INTEGER,
+                eissn INTEGER,
+                epub_date TEXT,
+                journal_full_name TEXT,
+                journal TEXT,
+                pub_date TEXT,
+                pub_type TEXT,
+                abstract TEXT,
+                keywords TEXT,
+                pub_status TEXT,
+                mesh_term TEXT,
+                fulltext_link TEXT,
+                topic TEXT
+            );
+        """)
+        self.conn.commit()
+        self.logger.info("Table created successfully")
+
+    def execute_sql(self, sql, data=None):
+        """
+        执行 SQL 语句，并返回操作的状态。
+        """
+        result = {'status': 'error', 'message': ''}
+
+        try:
+            if data:
+                self.cursor.execute(sql, data)
+            else:
+                self.cursor.execute(sql)
+            self.conn.commit()
+            result['status'] = 'success'
+            result['message'] = 'Operation completed successfully'
+        except Exception as e:
+            self.conn.rollback()
+            result['message'] = str(e)
+
+        self.logger.info(f'Execute SQL: {result}')
+        return result
+
+    def insert_publication(self, publication):
+        """
+        插入一条出版物记录到 'publications' 表中。
+        """
+        keys = ', '.join(self.publication_columns)
+        placeholders = ', '.join(['?'] * len(self.publication_columns))
+        sql = f'INSERT INTO {self.table_name} ({keys}) VALUES ({placeholders})'
+        return self.execute_sql(sql, publication)
+
+    def get_publication_by_doi(self, doi):
+        """
+        根据 DOI 查询出版物。
+        """
+        sql = "SELECT * FROM publications WHERE doi=?"
+        self.cursor.execute(sql, (doi,))
+        row = self.cursor.fetchone()
+        if row:
+            self.logger.info(f"Found publication with DOI {doi}")
+            return {'status': 'success', 'message': 'Publication found', 'data': row}
+        else:
+            self.logger.info(f"No publication found with DOI {doi}")
+            return {'status': 'error', 'message': 'Publication not found'}
+
+    def update_publication(self, doi, new_data):
+        """
+        更新给定 DOI 的出版物信息。
+        """
+        set_clause = ', '.join([f"{k}=?" for k in self.publication_columns[:-1]])  # Exclude 'doi' from the set clause
+        sql = f"UPDATE {self.table_name} SET {set_clause} WHERE doi=?"
+        return self.execute_sql(sql, (*new_data, doi))
+
+    def delete_publication(self, doi):
+        """
+        删除给定 DOI 的出版物。
+        """
+        sql = "DELETE FROM publications WHERE doi=?"
+        return self.execute_sql(sql, (doi,))
+
+    def close(self):
+        self.conn.close()
+        self.logger.info('Database connection closed')
+
+
+class SampleAccess:
+    def __init__(self, db_name):
+        self.conn = sqlite3.connect(db_name)
+        self.cursor = self.conn.cursor()
+        self.table_name = 'samples'
+        self.setup_logging()
+        self.create_table()
+
+    def setup_logging(self):
+        logging.basicConfig(filename='data_access.log', level=logging.INFO,
+                            format='%(asctime)s - %(levelname)s - %(message)s')
+        self.logger = logging.getLogger(__name__)
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(logging.INFO)
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+        console_handler.setFormatter(formatter)
+        self.logger.addHandler(console_handler)
+
+    def create_table(self):
+        """
+        创建一个名为 'samples' 的表，并设置 'source_sample_id' 为主键。
+        """
+        self.cursor.execute("""
+            CREATE TABLE IF NOT EXISTS samples (
+                source_sample_id TEXT PRIMARY KEY,
+                project_id TEXT,
+                datasetID TEXT,
+                experiment_id TEXT,
+                experiment_description TEXT,
+                protocol_description TEXT,
+                species TEXT,
+                species_id INTEGER,
+                sample_id TEXT,
+                sample_name TEXT,
+                sample_description TEXT,
+                organ TEXT,
+                organ_ontology_id TEXT,
+                organ_tax2 TEXT,
+                organ_tax2_ontology TEXT,
+                organ_note TEXT,
+                individual_id TEXT,
+                individual_name TEXT,
+                gender TEXT,
+                age INTEGER,
+                age_unit TEXT,
+                development_stage TEXT,
+                individual_status TEXT,
+                ethnic_group TEXT,
+                current_diagnostic TEXT,
+                phenotype TEXT,
+                treatment TEXT,
+                treatment_duration TEXT,
+                disease_note TEXT,
+                disease TEXT,
+                disease_id TEXT,
+                library_strategy TEXT,
+                library_layout TEXT,
+                library_selection TEXT,
+                technology_name TEXT,
+                tech_company TEXT,
+                release_time TEXT,
+                sequencer_name TEXT,
+                sequencer_company TEXT,
+                extraction_protocol TEXT,
+                data_processing TEXT,
+                source_name TEXT,
+                is_cancer BOOLEAN,
+                sample_location TEXT
+            );
+        """)
+        self.conn.commit()
+        self.logger.info("Table created successfully")
+
+    def insert_sample(self, sample):
+        """
+        插入一条样本记录到 'samples' 表中。
+        """
+        try:
+            self.cursor.execute("""
+                INSERT INTO samples (source_sample_id, project_id, datasetID, experiment_id, experiment_description, 
+                protocol_description, species, species_id, sample_id, sample_name, sample_description, organ, 
+                organ_ontology_id, organ_tax2, organ_tax2_ontology, organ_note, individual_id, individual_name, gender, 
+                age, age_unit, development_stage, individual_status, ethnic_group, current_diagnostic, phenotype, 
+                treatment, treatment_duration, disease_note, disease, disease_id, library_strategy, library_layout, 
+                library_selection, technology_name, tech_company, release_time, sequencer_name, sequencer_company, 
+                extraction_protocol, data_processing, source_name, is_cancer, sample_location)
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,
+                        ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,
+                        ?, ?, ?, ?);
+            """, sample)
+            self.conn.commit()
+            self.logger.info(f"Sample {sample[0]} inserted successfully")
+            return {'status': 'success', 'message': f'Sample {sample[0]} inserted successfully'}
+        except sqlite3.IntegrityError as e:
+            self.logger.error(f"Failed to insert sample {sample[0]}: {e}")
+            return {'status': 'error', 'message': f'Failed to insert sample {sample[0]}: {e}'}
+
+    def get_sample_by_id(self, source_sample_id):
+        """
+        根据 source_sample_id 查询样本。
+        """
+        self.cursor.execute(f"SELECT * FROM {self.table_name} WHERE source_sample_id=?", (source_sample_id,))
+        row = self.cursor.fetchone()
+        if row:
+            self.logger.info(f"Found sample {source_sample_id}")
+            return {'status': 'success', 'sample': row}
+        else:
+            self.logger.info(f"No sample found with: {source_sample_id}")
+            return {'status': 'error', 'message': f"No sample found with: {source_sample_id}"}
+
+    def update_sample(self, source_sample_id, new_data):
+        """
+        更新给定 source_sample_id 的样本信息。
+        """
+        try:
+            self.cursor.execute(f"""
+                UPDATE {self.table_name} SET 
+                    source_sample_id=?, project_id=?, datasetID=?, experiment_id=?, experiment_description=?, 
+                    protocol_description=?, species=?, species_id=?, sample_id=?, sample_name=?, sample_description=?, 
+                    organ=?, organ_ontology_id=?, organ_tax2=?, organ_tax2_ontology=?, organ_note=?, individual_id=?, 
+                    individual_name=?, gender=?, age=?, age_unit=?, development_stage=?, individual_status=?, 
+                    ethnic_group=?, current_diagnostic=?, phenotype=?, treatment=?, treatment_duration=?, 
+                    disease_note=?, disease=?, disease_id=?, library_strategy=?, library_layout=?, library_selection=?, 
+                    technology_name=?, tech_company=?, release_time=?, sequencer_name=?, sequencer_company=?, 
+                    extraction_protocol=?, data_processing=?, source_name=?, is_cancer=?, sample_location=?
+                WHERE source_sample_id=?
+            """, (*new_data, source_sample_id))
+            self.conn.commit()
+            self.logger.info(f"Sample {source_sample_id} updated successfully")
+            return {'status': 'success', 'message': f'Sample {source_sample_id} updated successfully'}
+        except sqlite3.Error as e:
+            self.logger.error(f"Failed to update sample {source_sample_id}: {e}")
+            return {'status': 'error', 'message': f'Failed to update sample {source_sample_id}: {e}'}
+
+    def delete_sample(self, source_sample_id):
+        """
+        删除给定 source_sample_id 的样本。
+        """
+        try:
+            self.cursor.execute(f"DELETE FROM {self.table_name} WHERE source_sample_id=?", (source_sample_id,))
+            self.conn.commit()
+            self.logger.info(f"Sample {source_sample_id} deleted successfully")
+            return {'status': 'success', 'message': f'Sample {source_sample_id} deleted successfully'}
+        except sqlite3.Error as e:
+            self.logger.error(f"Failed to delete sample {source_sample_id}: {e}")
+            return {'status': 'error', 'message': f'Failed to delete sample {source_sample_id}: {e}'}
+
+    def close(self):
+        self.conn.close()
+        self.logger.info('Database connection closed')
+
+
+if __name__ == "__main__":
+    # data_access = ProjectAccess('publications.db')
+    #
+    # # 插入示例数据
+    # publication = (
+    # '10.1038/s41590-022-01165-7', 34925200, None, 'dataset1', 'Title of the article', 'Author Name', 12345, 123456,
+    # '2022-07-27', 'Full Journal Name', 'Journal Abbreviation', '2022-07-27', 'Article', 'Abstract of the article',
+    # 'keyword1, keyword2', 'pub_status', 'mesh_term', 'fulltext_link', 'topic')
+    # data_access.insert_publication(publication)
+    #
+    # # 查询数据
+    # publication = data_access.get_publication_by_doi('10.1038/s41590-022-01165-7')
+    # print(publication)
+    #
+    # # 更新数据
+    # new_data = (34925201, None, 'dataset2', 'Updated Title', 'Updated Author', 12346, 123457, '2022-07-28',
+    #             'Updated Full Journal Name', 'Updated Journal Abbreviation', '2022-07-28', 'Review Article',
+    #             'Updated Abstract', 'updated_keyword1, updated_keyword2', 'updated_pub_status', 'updated_mesh_term',
+    #             'updated_fulltext_link', 'updated_topic')
+    # data_access.update_publication('10.1038/s41590-022-01165-7', new_data)
+    #
+    # # 删除数据
+    # # data_access.delete_publication('10.1038/s41590-022-01165-7')
+    #
+    # # 关闭连接
+    # data_access.close()
+
+    test_data = {
+        "source_sample_id": "SS123456",
+        "project_id": "PRJ1234",
+        "datasetID": "DS001",
+        "experiment_id": "EXP12345",
+        "experiment_description": "RNA sequencing of human liver tissue",
+        "protocol_description": "Illumina TruSeq RNA Library Prep",
+        "species": "Homo sapiens",
+        "species_id": 9606,
+        "sample_id": "SMP123456",
+        "sample_name": "Liver Tissue Sample 1",
+        "sample_description": "Fresh frozen liver biopsy from a patient with hepatocellular carcinoma",
+        "organ": "Liver",
+        "organ_ontology_id": "UBERON:0002107",
+        "organ_tax2": "liver",
+        "organ_tax2_ontology": "UBERON:0002107",
+        "organ_note": "Sample taken from the right lobe",
+        "individual_id": "INDV12345",
+        "individual_name": "Patient 1",
+        "gender": "Male",
+        "age": 45,
+        "age_unit": "years",
+        "development_stage": "Adult",
+        "individual_status": "Cancer patient",
+        "ethnic_group": "Caucasian",
+        "current_diagnostic": "Hepatocellular carcinoma",
+        "phenotype": "Hepatomegaly",
+        "treatment": "Sorafenib",
+        "treatment_duration": "3 months",
+        "disease_note": "Stage IIIA",
+        "disease": "Hepatocellular carcinoma",
+        "disease_id": "DOID:9352",
+        "library_strategy": "RNA-Seq",
+        "library_layout": "PAIRED",
+        "library_selection": "polyA",
+        "technology_name": "NextSeq 500",
+        "tech_company": "Illumina",
+        "release_time": "2023-08-01",
+        "sequencer_name": "NextSeq 500",
+        "sequencer_company": "Illumina",
+        "extraction_protocol": "QIAzol Lysis Reagent",
+        "data_processing": "Trimming and alignment using HISAT2",
+        "source_name": "Hospital A",
+        "is_cancer": True,
+        "sample_location": "Right lobe of liver"
+    }
+    test_values = tuple(test_data.values())
+    data_access = SampleAccess('sample.db')
+    data_access.insert_sample(test_values)
+    data = data_access.get_sample_by_id('SS123456')
+    print(data)
+    data_access.update_sample('SS123456', test_values)
--- BSM/DataController/data_controller.py
+++ BSM/DataController/data_controller.py
@@ -1,4 +1,5 @@
 from BSM.DataAccess import DataAccess
+from BSM.DataAccess.data_access import ProjectAccess, SampleAccess
 
 
 class DataController:
@@ -19,15 +20,107 @@ class DataController:
         self.data_access.close()
 
 
+class ProjectController:
+    def __init__(self, db_name='../../DBS/test.db'):
+        self.data_access = ProjectAccess(db_name)
+
+    def insert_project(self, publication):
+        try:
+            if isinstance(publication, dict):
+                publication = tuple(publication.values())
+            result = self.data_access.insert_publication(publication)
+            if result['status'] == 'success':
+                result['data'] = publication
+                return result
+            else:
+                return result
+        except Exception as e:
+            return 'insert project failed'
+
+    def close(self):
+        self.data_access.close()
+
+
+class SampleController:
+    def __init__(self, db_name='../../DBS/test.db'):
+        self.data_access = SampleAccess(db_name)
+
+    def insert_sample(self, sample):
+        try:
+            if isinstance(sample, dict):
+                sample = tuple(sample.values())
+            result = self.data_access.insert_sample(sample)
+            if result['status'] == 'success':
+                result['data'] = sample
+                return result
+            else:
+                return result
+        except Exception as e:
+            return 'insert project failed'
+
+    def close(self):
+        self.data_access.close()
+
 # Example usage
 if __name__ == "__main__":
-    db_name = '../../DBS/test.db'
-    controller = DataController(db_name)
-    data = controller.read_all_data()
-    if data is not None:
-        print("Data retrieved successfully:")
-        for row in data:
-            print(row)
-    else:
-        print("Failed to retrieve data.")
-    controller.close()
+    db_name = '../../DBS/projects.db'
+    # insert sample
+    test_sample= {
+        "source_sample_id": "SS1234567",
+        "project_id": "PRJ1234",
+        "datasetID": "DS001",
+        "experiment_id": "EXP12345",
+        "experiment_description": "RNA sequencing of human liver tissue",
+        "protocol_description": "Illumina TruSeq RNA Library Prep",
+        "species": "Homo sapiens",
+        "species_id": 9606,
+        "sample_id": "SMP123456",
+        "sample_name": "Liver Tissue Sample 1",
+        "sample_description": "Fresh frozen liver biopsy from a patient with hepatocellular carcinoma",
+        "organ": "Liver",
+        "organ_ontology_id": "UBERON:0002107",
+        "organ_tax2": "liver",
+        "organ_tax2_ontology": "UBERON:0002107",
+        "organ_note": "Sample taken from the right lobe",
+        "individual_id": "INDV12345",
+        "individual_name": "Patient 1",
+        "gender": "Male",
+        "age": 45,
+        "age_unit": "years",
+        "development_stage": "Adult",
+        "individual_status": "Cancer patient",
+        "ethnic_group": "Caucasian",
+        "current_diagnostic": "Hepatocellular carcinoma",
+        "phenotype": "Hepatomegaly",
+        "treatment": "Sorafenib",
+        "treatment_duration": "3 months",
+        "disease_note": "Stage IIIA",
+        "disease": "Hepatocellular carcinoma",
+        "disease_id": "DOID:9352",
+        "library_strategy": "RNA-Seq",
+        "library_layout": "PAIRED",
+        "library_selection": "polyA",
+        "technology_name": "NextSeq 500",
+        "tech_company": "Illumina",
+        "release_time": "2023-08-01",
+        "sequencer_name": "NextSeq 500",
+        "sequencer_company": "Illumina",
+        "extraction_protocol": "QIAzol Lysis Reagent",
+        "data_processing": "Trimming and alignment using HISAT2",
+        "source_name": "Hospital A",
+        "is_cancer": True,
+        "sample_location": "Right lobe of liver"
+    }
+    test_values = tuple(test_sample.values())
+    controller = SampleController(db_name)
+    res = controller.insert_sample(test_values)
+    print(res)
+
+    # insert project
+    test_publication = (
+    '10.1038/s41590-022-01165-333', 34925200, None, 'dataset1', 'Title of the article', 'Author Name', 12345, 123456,
+    '2022-07-27', 'Full Journal Name', 'Journal Abbreviation', '2022-07-27', 'Article', 'Abstract of the article',
+    'keyword1, keyword2', 'pub_status', 'mesh_term', 'fulltext_link', 'topic')
+    controller = ProjectController(db_name)
+    res = controller.insert_project(test_publication)
+    print(res)
