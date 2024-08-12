0e50e34d62e9dfc40a6de6025038ff1e5ea9e511
Relaxxxxx <liu960929@163.com>
2024-08-12 14:26:35 +0800

    bug fix

--- BSM/Fetcher/utils/json_file.py
+++ BSM/Fetcher/utils/json_file.py
@@ -1,6 +1,8 @@
 import json
 import ijson
-
+from pandas import json_normalize
+import pandas as pd
+import os
 class JsonManager(object):
     def __init__(self,  filename):
         self.filename = filename
@@ -27,3 +29,18 @@ class JsonManager(object):
             for item in ijson.items(f, 'item'):
                 items.append(item)
         return items
+
+    def json_to_csv(self, folder_path, outpath, csv_name, max_length=35000):
+        with open(folder_path, 'r') as f:
+            data = json.load(f)
+
+        flat_data = json_normalize(data)
+        filtered_data = flat_data[flat_data.apply(lambda row: row.astype(str).str.len().sum() <= max_length, axis=1)]
+        exceed_data = flat_data[flat_data.apply(lambda row: row.astype(str).str.len().sum() > max_length, axis=1)]
+        if not exceed_data.empty:
+            exceed_data = exceed_data.iloc[:, 0:8]
+        result_data = pd.concat([filtered_data, exceed_data], ignore_index=True)
+
+        output_file_path = os.path.join(outpath, csv_name)
+        result_data.to_csv(output_file_path, index=False)
+        print(f"Data has been saved to {output_file_path}")
