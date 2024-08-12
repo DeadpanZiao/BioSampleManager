e1146c58f97c0cff90fb76718ff9524930a07947
Relaxxxxx <liu960929@163.com>
2024-08-12 14:35:11 +0800

    bug fix

--- BSM/Fetcher/utils/json_file.py
+++ BSM/Fetcher/utils/json_file.py
@@ -50,8 +50,8 @@ class JsonManager(object):
 
         return json_list
 
-    def json_to_csv(self, folder_path, outpath, csv_name, max_length=35000):
-        with open(folder_path, 'r') as f:
+    def json_to_csv(self, json_path, output_path, csv_name, max_length=35000):
+        with open(json_path, 'r') as f:
             data = json.load(f)
 
         flat_data = json_normalize(data)
@@ -61,6 +61,6 @@ class JsonManager(object):
             exceed_data = exceed_data.iloc[:, 0:8]
         result_data = pd.concat([filtered_data, exceed_data], ignore_index=True)
 
-        output_file_path = os.path.join(outpath, csv_name)
+        output_file_path = os.path.join(output_path, csv_name)
         result_data.to_csv(output_file_path, index=False)
         print(f"Data has been saved to {output_file_path}")
