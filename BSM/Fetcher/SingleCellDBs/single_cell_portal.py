3b173b0312c80ecd008c96476dc470ecc125cd39
Relaxxxxx <liu960929@163.com>
2024-08-14 10:16:34 +0800

    url update

--- BSM/Fetcher/SingleCellDBs/single_cell_portal.py
+++ BSM/Fetcher/SingleCellDBs/single_cell_portal.py
@@ -14,22 +14,22 @@ class SingleCellPortalFetcher(SingleCellDBFetcher):
         response = requests.get(self.datasets_url, headers=self.headers, verify=False)
         if response.status_code == 200:
             studies = response.json()
-            manager = JsonManager(db_name)
-            manager.save(studies)
-            self.logger.info("Data saved successfully to JSON file.")
-            merged_data = {}
+            final_data = []
             for study in studies:
                 accessions = study.get('accession', 'N/A')
                 study_url = f"{self.datasets_url}/{accessions}"
                 response = requests.get(study_url, headers=self.headers, verify=False)
                 if response.status_code == 200:
                     study_data = response.json()
-                    merged_data.update(study_data)
+                    final_data.append(study_data)
                     self.logger.info(f"Data saved successfully to {accessions}.json file.")
                 else:
                     self.logger.error(f"Failed to retrieve study {accessions}. Status code: {response.status_code}")
-            manager = JsonManager('date')
-            manager.save(merged_data)
+            manager = JsonManager(db_name)
+            manager.save(final_data)
         else:
             self.logger.error(f"Failed to retrieve studies. Status code: {response.status_code}")
+
+f = SingleCellPortalFetcher()
+f.fetch(r'scp.json')
 
