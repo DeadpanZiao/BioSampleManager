42b8b5a70a8da9e0ab2746f631989f0b7f79a529
余茜茜 <yuxixi@zhejianglab.org>
2024-08-26 15:59:24 +0800

    添加下载soft文件的.py&对应的测试.py

--- tests/test-fetcher/test_download_softfile.py
+++ tests/test-fetcher/test_download_softfile.py
@@ -7,7 +7,7 @@ downloader = DownloadSoft(soft_filepath='D:/3.3-zjprogram/BioDatabase/GEO/GEO_so
 # downloader.download_soft_failed_dl()
 
 # 下载最新爬的id
-# downloader.download_soft_new()
+downloader.download_soft_new()
 
 # 将新爬的ids添加倒gse_ids.txt中
 downloader.save_as_gse_ids_file_new()
